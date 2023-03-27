bb41746d294699e1fbee3f706986bc9a0adf93ce
RobinsonXavier <robinson.webzen@gmail.com>
2023-03-26 21:08:27 -0300

    Finished

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        List<String> array1 = new ArrayList<>();
+        List<String> array2 = new ArrayList<>();
+        String resultado = "";
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Escreva uma palavra:\n");
+            String word = scan.nextLine();
+
+            System.out.println("Escreva outra palavra:\n");
+            String word2 = scan.nextLine();
+
+            array1.add(word);
+            array2.add(word2);
+        }
+
+        for(int i = 0; i < array1.size(); i++) {
+            for(int j = 0; j < array2.size(); j++) {
+                if(array1.get(i).equals(array2.get(j)) ) {
+                    resultado = resultado + " " + array1.get(i);
+                }
+            }
+        }
+
+        System.out.println("A resposta é: " + resultado);
+
     }
 }
